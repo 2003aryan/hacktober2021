117aaeeed4de2be7898fa4871f9107e6c04c5b10
tb-rules10 <58645688+tb-rules10@users.noreply.github.com>
2021-10-05 15:03:31 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class helloworld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("TB Here!");
+        System.out.println("Hello World!");
+    }
+}
