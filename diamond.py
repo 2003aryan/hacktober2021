770acac1106e67fe3ac1236747b11fa11f3e1b42
priyaaa404 <92673866+priyaaa404@users.noreply.github.com>
2021-10-30 20:22:29 +0530

    Create diamond.py

---
+++ diamond.py
@@ -0,0 +1,13 @@
+n = 5
+
+print("Pattern 1")
+
+for a1 in range (0,n):
+    for a2 in range (a1):
+        print("*", end="")
+    print()
+
+for a1 in range (n,0,-1):
+    for a2 in range (a1):
+        print("*", end="")
+    print()
